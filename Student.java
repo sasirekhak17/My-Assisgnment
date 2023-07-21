1a9e015bb79e87a23d14e2ae0731e7d6ae53d17f
sasirekhak17 <139632820+sasirekhak17@users.noreply.github.com>
2023-07-21 09:02:59 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+public class Student {
+
+		
+		// TODO Auto-generated method stub
+		 String studentName ="Sasirekha";
+		 int rollNumber = 454;
+		 String collegeName="Fatima College";
+		 int markScored=403;
+		 float cgpa = 8.3F;	 
+		 
+				}
+
