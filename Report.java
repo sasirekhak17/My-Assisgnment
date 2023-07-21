97422d32cb99dd5af761424e945dee7c26e3d020
sasirekhak17 <139632820+sasirekhak17@users.noreply.github.com>
2023-07-21 09:02:37 +0530

    Add files via upload

---
+++ Report.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class Report {
+
+	public static void main(String[] args) {
+		Student std=new Student();
+		System.out.println("Student Name :"+std.studentName);
+		
+		System.out.println("Roll Number :" +std.markScored);
+		
+		System.out.println("College name :" +std.collegeName);
+		
+		System.out.println("Marks Scored :" +std.markScored);
+		
+		System.out.println("CGPA : "+std.cgpa);
+		
+		
+
+	}
+
+}
