9c54b177b241e190e05ec7e31468db9e7547b392
sasirekhak17 <139632820+sasirekhak17@users.noreply.github.com>
2023-07-21 09:01:32 +0530

    Add files via upload

---
+++ Bike.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class Bike {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Car car=new Car();
+Bike bike=new Bike();
+
+car.applyBreak();
+car.soundHorn();
+
+bike.applyBreak();
+bike.soundHorn();
+	}
+
+}
